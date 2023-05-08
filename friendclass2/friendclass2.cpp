a0f65b2997906ea4e2cba5d0e8e7ccdf2334771f
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-05-08 15:49:27 +0700

    membuat class mahasiswa

--- friendclass2/friendclass2.cpp
+++ friendclass2/friendclass2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa& a, string);
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
